10a4260a20bcce2145dae2f95162f9bb689c06f3
dp <codewithdp2073@gmail.com>
2025-04-25 15:27:31 +0530

    Refactor message handling in App component; remove hardcoded messages and implement input state for user messages

--- src/App.jsx
+++ src/App.jsx
@@ -9,21 +9,10 @@ const App = () => {
       role: "system",
       content: "How can i help you?",
     },
-    {
-      role: "user",
-      content: "Hello",
-    },
-    {
-      role: "assistant",
-      content: "Hello",
-    },
-    {
-      role: "model",
-      content: "How can i help you?",
-    },
   ]);
   const [loading, setLoading] = useState(false);
   const [engine, setEngine] = useState(null);
+  const [input, setInput] = useState("");
   useEffect(() => {
     const selectedModel = "Llama-2-7b-chat-hf-q4f32_1-MLC";
     webllm
@@ -36,15 +25,48 @@ const App = () => {
         setEngine(engine);
       })
       .catch((err) => {
-        console.log("Failed to create engine", err);
+        console.log("Failed to create engine: ", err);
       });
   }, []);
 
+  function sendMessageToLLM() {
+    setLoading(true);
+    if (input === "") return setLoading(false);
+    const tempMessage = [...messages];
+    // console.log("temp messagges:", tempMessage);
+    tempMessage.push({
+      role: "user",
+      content: input,
+    });
+    setMessages(tempMessage);
+    // setMessages([...messages], {
+    //   role: "user",
+    //   content: input,
+    // });
+    setInput("");
+    setLoading(false);
+
+    // Reply from LLM:
+    engine.chat.completions
+      .create({
+        messages: tempMessage,
+      })
+      .then((reply) => {
+        console.log(reply);
+        console.log("reply", reply.choices[0].message);
+        // console.log(reply.usage);
+        const text = reply.choices[0].message.content;
+        setMessages([...tempMessage], {
+          role: "system",
+          content: text,
+        });
+      });
+  }
   return (
     <main>
       <section>
         <div className="conversation-area">
-        <div className="logo">WebLLM</div>
+          <div className="logo">WebLLM</div>
           <div className="messages">
             {messages.map((msg, idx) => {
               return (
@@ -55,8 +77,22 @@ const App = () => {
             })}
           </div>
           <div className="input-area">
-            <input type="text" placeholder="Message LLm " />
-            <button>Send</button>
+            <input
+              type="text"
+              value={input}
+              placeholder="Message LLm "
+              onChange={(e) => {
+                setInput(e.target.value);
+              }}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  sendMessageToLLM();
+                }
+              }}
+            />
+            <button onClick={() => sendMessageToLLM()}>
+              {loading ? <div>Loading....</div> : <div>Send</div>}
+            </button>
           </div>
         </div>
       </section>
