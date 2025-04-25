ce5f2e2f91357d21b04ea6ccc7ccd08f4d718030
dp <codewithdp2073@gmail.com>
2025-04-25 12:05:35 +0530

    Add conversation UI and styling; update dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.css";
+import "./app.scss";
 
 const App = () => {
-  return <div>App</div>;
+  const [messages, setMessages] = useState([
+    {
+      role: "system",
+      content: "How can i help you?",
+    },
+    {
+      role: "user",
+      content: "Hello",
+    },
+    {
+      role: "assistant",
+      content: "Hello",
+    },
+    {
+      role: "model",
+      content: "How can i help you?",
+    },
+  ]);
+  return (
+    <main>
+      <section>
+        <div className="conversation-area">
+          <div className="messages">
+            {messages.map((msg, idx) => {
+              return (
+                <div key={idx} className={`message ${msg.role}`}>
+                  {msg.content}
+                </div>
+              );
+            })}
+          </div>
+          <div className="input-area">
+            <input type="text" placeholder="Message LLm " />
+            <button>Send</button>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default App;
